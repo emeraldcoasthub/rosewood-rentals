f193e738c3d8adf8b0c61dea2b6bddfcac3fd8b1
Patrick Black <pj@perfectimprints.com>
2019-11-27 12:07:22 -0600

    init

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="container">
+    <b-row>
+      <b-col cols="12" lg="8" md="6"> </b-col>
+    </b-row>
+  </section>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
